4bf6bbffb779dcb454cd30a70668779fcda0e029
LiYingxuan <mvp_xuan@163.com>
2019-03-30 17:00:01 +0800

    Update index and banner.

--- src/components/main/Banner.vue
+++ src/components/main/Banner.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="banner-container">
-    <el-carousel :interval="4000" type="card" height="200px">
+    <el-carousel type="card" height="210px">
       <el-carousel-item v-for="item in 3" :key="item">
-        <img width="100%" :src="banners.img" @click="toUrl(banners.url)" />
+        <img height="100%" :src="banners.img" @click="toUrl(banners.url)" />
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -10,7 +10,6 @@
 
 <script>
 export default {
-  name: "MainPageBanner",
   data() {
     return {
       banners: {
@@ -32,19 +31,5 @@ export default {
 <style rel="stylesheet/scss" lang="scss" scoped>
 .banner-container {
   margin-top: 30px;
-}
-
-.el-carousel__item h3 {
-  color: #475669;
-  font-size: 14px;
-  opacity: 0.75;
-  line-height: 200px;
-  margin: 0;
-}
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
 }
 </style>
