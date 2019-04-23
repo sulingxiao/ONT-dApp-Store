ffcd55fa1e0352750a64bdfd173c0801be7d04f9
LiYingxuan <mvp_xuan@163.com>
2019-04-23 16:32:28 +0800

    Update some code.

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,19 +1,3 @@
-/*
- * **************************************************************************************
- *  Copyright © 2014-2018 Ontology Foundation Ltd.
- *  All rights reserved.
- *
- *  This software is supplied only under the terms of a license agreement,
- *  nondisclosure agreement or other written agreement with Ontology Foundation Ltd.
- *  Use, redistribution or other disclosure of any parts of this
- *  software is prohibited except in accordance with the terms of such written
- *  agreement with Ontology Foundation Ltd. This software is confidential
- *  and proprietary information of Ontology Foundation Ltd.
- *
- * **************************************************************************************
- *
- */
-
 import Vue from "vue";
 import VueI18n from "vue-i18n";
 import elementEnLocale from "element-ui/lib/locale/lang/en"; // element-ui lang
@@ -36,10 +20,7 @@ const messages = {
 };
 
 const i18n = new VueI18n({
-  // set locale
-  // options: en | zh | es
   locale: LangStorage.getLang("en"),
-  // set locale messages
   messages
 });
 
